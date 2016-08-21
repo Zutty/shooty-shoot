425f5d9e6e153752add43e2ea5f5d5a313fdf11f
Zutty <george.weller@gmail.com>
2016-08-21 20:24:52 +0100

    Switch to visible text metafiles. Rebuild light. Merge missing changes from FPS project.

---
+++ Assets/scripts/WeaponController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour {
+
+	public Image crosshair;
+	public Color highlightColour = Color.red;
+	public float fireRate = 0.5f;
+	public float damage = 5f;
+
+	private Camera mainCamera;
+	private Color defaultColour;
+	private float fireTimer = 0f;
+
+	void Start () {
+		mainCamera = GetComponentInChildren<Camera> ();
+		defaultColour = crosshair.color;
+	}
+	
+	void Update () {
+		RaycastHit hit;
+
+		if (Physics.Raycast (mainCamera.ViewportPointToRay (new Vector3 (.5F, .5F, 0)), out hit)) {
+			var hitEnemy = hit.transform.tag == "Enemy";
+
+			crosshair.color = hitEnemy ? highlightColour : defaultColour;
+
+			if(hitEnemy && Input.GetButtonDown("Fire1") && Time.time > fireTimer) {
+				fireTimer = Time.time + fireRate;
+				hit.collider.SendMessageUpwards("OnHit", damage);
+			}
+		}
+	}
+}
