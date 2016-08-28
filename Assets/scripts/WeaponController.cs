16d78888b3a05913efae4cf0fe21ddc51709462f
Zutty <george.weller@gmail.com>
2016-08-28 17:31:24 +0100

    Add crude gun sound effect

--- Assets/scripts/WeaponController.cs
+++ Assets/scripts/WeaponController.cs
@@ -8,18 +8,25 @@ public class WeaponController : MonoBehaviour {
 	public Color highlightColour = Color.red;
 	public float fireRate = 0.5f;
 	public float damage = 5f;
+	public AudioClip shootSound;
 
 	private Camera mainCamera;
 	private Color defaultColour;
 	private float fireTimer = 0f;
+	private AudioSource audioSource;
 
 	void Start () {
 		mainCamera = GetComponentInChildren<Camera> ();
+		audioSource = GetComponent<AudioSource>();
 		defaultColour = crosshair.color;
 	}
 	
 	void Update () {
 		RaycastHit hit;
+
+		if (Input.GetButtonDown ("Fire1") && Time.time > fireTimer) {
+			audioSource.PlayOneShot (shootSound, Random.Range (.5f, 1f));
+		}
 
 		if (Physics.Raycast (mainCamera.ViewportPointToRay (new Vector3 (.5F, .5F, 0)), out hit)) {
 			var hitEnemy = hit.transform.tag == "Enemy";
@@ -27,6 +34,7 @@ public class WeaponController : MonoBehaviour {
 			crosshair.color = hitEnemy ? highlightColour : defaultColour;
 
 			if(hitEnemy && Input.GetButtonDown("Fire1") && Time.time > fireTimer) {
+				audioSource.PlayOneShot (shootSound, Random.Range (.5f, 1f));
 				fireTimer = Time.time + fireRate;
 				hit.collider.SendMessageUpwards("OnHit", damage);
 			}
