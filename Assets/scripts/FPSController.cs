aa29d9a86827e4047f500051738e653ab806fa1f
Zutty <george.weller@gmail.com>
2016-08-28 17:39:12 +0100

    Add terrible camera bob effect

--- Assets/scripts/FPSController.cs
+++ Assets/scripts/FPSController.cs
@@ -9,11 +9,15 @@ public class FPSController : MonoBehaviour {
 	
 	private CharacterController controller;
 	private Vector3 motion = Vector3.zero;
+
+	private float _bobTimer;
+	private Camera _camera;
 	
 	void Start () {
 		controller = GetComponent<CharacterController> ();
+		_camera = GetComponentInChildren<Camera> ();
 	}
-	
+
 	void Update () {
 		if(controller.isGrounded) {
 			motion = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
@@ -25,5 +29,21 @@ public class FPSController : MonoBehaviour {
 		}
 		motion.y -= gravity * Time.deltaTime;
 		controller.Move(motion * Time.deltaTime);
+
+		Vector3 v = controller.velocity;
+		v.y = 0;
+
+		float bob = 0f;
+
+		if (v.magnitude > 0.001f) {
+			bob = Mathf.Sin (2f * Mathf.PI * v.magnitude * _bobTimer * 0.15f);
+			_bobTimer += Time.deltaTime;
+		} else {
+			_bobTimer = 0f;
+		}
+
+		Vector3 b = new Vector3 ();
+		b.y = 0.88f + (bob * 0.05f);
+		_camera.transform.localPosition = b;
 	}
 }
