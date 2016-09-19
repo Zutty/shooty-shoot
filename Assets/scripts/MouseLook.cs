e536ad66ebe591b20d9e17570d3ea6d76e03b5c8
Zutty <george.weller@gmail.com>
2016-09-20 00:30:48 +0100

    Restrict vertical camera movement

--- Assets/scripts/MouseLook.cs
+++ Assets/scripts/MouseLook.cs
@@ -5,18 +5,21 @@ public class MouseLook : MonoBehaviour {
 
 	public float sensitivity = 2F;
 
-	private Vector3 direction;
+	private Vector3 euler;
 
 	void Start () {
-		direction = transform.localRotation.eulerAngles;
+		euler = transform.localRotation.eulerAngles;
 	}
 	
 	void Update () {
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 
-		direction.y += Input.GetAxisRaw ("Mouse X") * sensitivity;
-		direction.x -= Input.GetAxisRaw ("Mouse Y") * sensitivity;
-		transform.localRotation = Quaternion.Euler(direction);
+		euler.y += Input.GetAxisRaw ("Mouse X") * sensitivity;
+		euler.x -= Input.GetAxisRaw ("Mouse Y") * sensitivity;
+
+		euler.x = Mathf.Clamp (euler.x, -90f, 90f);
+
+		transform.localRotation = Quaternion.Euler(euler);
 	}
 }
