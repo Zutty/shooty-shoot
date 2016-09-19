0ba471d19f7cc046cf28aed357f83879bf364fd8
Zutty <george.weller@gmail.com>
2016-09-20 00:30:37 +0100

    Instantiate impact spark effect on weapon hit

---
+++ Assets/scripts/DestroyOnCompletion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DestroyOnCompletion : MonoBehaviour {
+
+	private ParticleSystem particles;
+
+	void Start () {
+		particles = GetComponent<ParticleSystem> ();
+	}
+	
+	void Update () {
+		if(!particles.IsAlive()) {
+			Destroy (gameObject);
+		}
+	}
+}
--- Assets/scripts/WeaponController.cs
+++ Assets/scripts/WeaponController.cs
@@ -9,6 +9,7 @@ public class WeaponController : MonoBehaviour {
 	public float fireRate = 0.5f;
 	public float damage = 5f;
 	public AudioClip shootSound;
+	public GameObject impactPrefab;
 
 	private Camera mainCamera;
 	private Color defaultColour;
@@ -25,24 +26,25 @@ public class WeaponController : MonoBehaviour {
 	}
 	
 	void Update () {
-		RaycastHit hit;
+		RaycastHit raycastHit;
+		var hit = Physics.Raycast (mainCamera.ViewportPointToRay (new Vector3 (.5F, .5F, 0)), out raycastHit);
+
+		var hitEnemy = hit && raycastHit.transform.tag == "Enemy";
+		crosshair.color = hitEnemy ? highlightColour : defaultColour;
 
 		muzzleFlash.SetActive (false);
 
 		if (Input.GetButtonDown ("Fire1") && Time.time > fireTimer) {
 			audioSource.PlayOneShot (shootSound, Random.Range (.5f, 1f));
+			fireTimer = Time.time + fireRate;
 			muzzleFlash.SetActive (true);
-		}
 
-		if (Physics.Raycast (mainCamera.ViewportPointToRay (new Vector3 (.5F, .5F, 0)), out hit)) {
-			var hitEnemy = hit.transform.tag == "Enemy";
+			GameObject impact = Instantiate (impactPrefab);
+			impact.transform.position = raycastHit.point;
+			impact.transform.rotation = Quaternion.FromToRotation (Vector3.forward, raycastHit.normal);
 
-			crosshair.color = hitEnemy ? highlightColour : defaultColour;
-
-			if(hitEnemy && Input.GetButtonDown("Fire1") && Time.time > fireTimer) {
-				audioSource.PlayOneShot (shootSound, Random.Range (.5f, 1f));
-				fireTimer = Time.time + fireRate;
-				hit.collider.SendMessageUpwards("OnHit", damage);
+			if(hitEnemy) {
+				raycastHit.collider.SendMessageUpwards("OnHit", damage);
 			}
 		}
 	}
